d9def14b5da56663e700bbf6e79fedc7a44ac5c9
sadek1999 <sadekali1530@gmail.com>
2024-06-26 21:56:36 +0600

    set store

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
-
 function App() {
-
-
   return (
     <>
-    <h1>React with redux .............</h1>
+      <div className="flex justify-center items-center h-screen w-full">
+        <div className="flex gap-2 justify-center items-center border border-red-200 p-5 bg-orange-100">
+          <button className="btn-primary bg-green-500 text-white p-2 rounded-md">increment</button>
+          <h1 className="text-xl mx-2">00</h1>
+          <button className="btn-primary bg-red-500 text-white p-2 rounded-md">Decrement</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
