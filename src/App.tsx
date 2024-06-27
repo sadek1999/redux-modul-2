78483877fef1613316d84668a586b5ad14622c90
sadek1999 <sadekali1530@gmail.com>
2024-06-27 06:55:19 +0600

    successfully create a counter with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+
+import { decrement, increment, incrementByValue } from "./redux/features/counterslices";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+
+  const {count}=useAppSelector((state)=>state.counter )
+  const dispatch=useAppDispatch()
+  
   return (
     <>
       <div className="flex justify-center items-center h-screen w-full">
         <div className="flex gap-2 justify-center items-center border border-red-200 p-5 bg-orange-100">
-          <button className="btn-primary bg-green-500 text-white p-2 rounded-md">increment</button>
-          <h1 className="text-xl mx-2">00</h1>
-          <button className="btn-primary bg-red-500 text-white p-2 rounded-md">Decrement</button>
+          <button      onClick={() => dispatch(increment())} className="btn-primary bg-green-500 text-white p-2 rounded-md">increment</button>
+          <button      onClick={() => dispatch(incrementByValue(5))} className="btn-primary bg-green-500 text-white p-2 rounded-md">increment by value</button>
+          <h1 className="text-xl mx-2">{count}</h1>
+          <button onClick={()=>dispatch(decrement())} className="btn-primary bg-red-500 text-white p-2 rounded-md">Decrement</button>
         </div>
       </div>
     </>
--- src/redux/features/counterslices.ts
+++ src/redux/features/counterslices.ts
@@ -1,9 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+type TCounter={
+    count:number
+}
+const initialState:TCounter = { count: 0 };
 
-const initialState={count:0}
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    incrementByValue:(state,action:PayloadAction<number>)=>{
+        state.count=state.count + action.payload;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+  },
+});
 
-const counterslice=createSlice({
-    name:'counter',
-    initialState,
-    reducers:{}
-})
+export const {increment,decrement ,incrementByValue}=counterSlice.actions
+
+export default counterSlice.reducer;
